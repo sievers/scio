90967f4b8390ff805b52c28d00df5a1b8f8fbaa8
Jonathan Sievers <jonathan.sievers@gmail.com>
2016-03-30 14:59:32 +0200

    rewrote scio to keep file pointer in memory.  hopefully more robust on pi.

--- scio.py
+++ scio.py
@@ -1,33 +1,87 @@
 import numpy
 import os
 
-def append(arr,fname,overwrite=False):
-    asdf='abc'
-    assert(type(fname)==type(asdf))
-    asdf=numpy.zeros(2)
-    assert(type(arr)==type(asdf))
-    if overwrite:
-        os.system('rm  ' + fname)
-        
-    if (os.path.isfile(fname)):
-        f=open(fname,'a')
-        arr.tofile(f)
-        f.close()
-    else:
-        print 'creating ' + fname
-        f=open(fname,'w')
+
+    
+
+class scio:
+    def __init__(self,fname,arr=None,status='w'):
+        self.fid=open(fname,status)
+        self.fname=fname
+        if arr is None:
+            self.dtype=None
+            self.shape=None
+            self.initialized=False
+        else:
+            self.dtype=arr.dtype
+            self.shape=arr.shape
+            self.initialized=True
+            self.write_header(arr)
+            self.append(arr)
+
+            
+    def __del__(self):
+        print 'closing scio file ' + self.fname
+        self.fid.flush()
+        self.fid.close()
+
+    def close(self):
+        self.__del__()
+    def write_header(self,arr):
         sz=arr.shape
         myvec=numpy.zeros(len(sz)+2,dtype='int32')
         myvec[0]=len(sz)
         for i in range(len(sz)):
             myvec[i+1]=sz[i]
         myvec[-1]=dtype2int(arr)
-        #print myvec
-        #print sz
-        #print type(myvec)
-        myvec.tofile(f)
-        arr.tofile(f)
-        f.close()
+        myvec.tofile(self.fid)
+        
+        
+    def append(self,arr):
+        if self.initialized==False:
+            self.dtype=arr.dtype
+            self.shape=arr.shape
+            self.write_header(arr)
+            self.initialized=True
+
+        if (arr.shape==self.shape):
+            pass
+        else:
+            print "shape mismatch in scio.append"
+        if (arr.dtype==self.dtype):
+            arr.tofile(self.fid)
+            self.fid.flush()
+        else:
+            print 'dtype mismatch in scio.append'
+        
+            
+#def append(arr,fname,overwrite=False):
+#    asdf='abc'
+#    assert(type(fname)==type(asdf))
+#    asdf=numpy.zeros(2)
+#    assert(type(arr)==type(asdf))
+#    if overwrite:
+#        os.system('rm  ' + fname)
+#        
+#    if (os.path.isfile(fname)):
+#        f=open(fname,'a')
+#        arr.tofile(f)
+#        f.close()
+#    else:
+#        print 'creating ' + fname
+#        f=open(fname,'w')
+#        sz=arr.shape
+#        myvec=numpy.zeros(len(sz)+2,dtype='int32')
+#        myvec[0]=len(sz)
+#        for i in range(len(sz)):
+#            myvec[i+1]=sz[i]
+#        myvec[-1]=dtype2int(arr)
+#        #print myvec
+#        #print sz
+#        #print type(myvec)
+#        myvec.tofile(f)
+#        arr.tofile(f)
+#        f.close()
 
 def read(fname):
     f=open(fname)
